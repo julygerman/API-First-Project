91abeb6baa7aa2d210679d117cde4697baf9f9cf
Julio Aleman <flash@Julios-MacBook-Pro.local>
2020-08-06 14:00:06 -0400

    firts update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// I need to create a board layout 5 X 6
+// each money box needs to have an event clicker
+// the board needs to have category specific API jeopardy questions attached to each indiviual category and its cards
+// the game needs to be able to keep score
+// the game needs to generate multiple choice questions per card
+// the game needs to be able to subtract points from the player if theier answers are false
+// if the player wins or loses the board needs to stop being clickable
+// the reset button nets to initialize a game reset
+// the game needs to be able to 
+// I would like to randomize the board with new questions and categories but it seems to complicated becasue i would ahve to come up with all the multiple choice answers
+
+
+
+// Write Objectives
+// Write User Stories
