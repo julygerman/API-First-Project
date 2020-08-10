9dab099415c431d8a083bb079ae7e132aabd34db
Julio Aleman <flash@Julios-MacBook-Pro.local>
2020-08-10 13:16:12 -0400

    add setStatusClass function

--- js/main.js
+++ js/main.js
@@ -121,7 +121,15 @@ if (correct) {
     }
 }
 
-
+function setStatusClass(element, correct){
+    clearStatusClass(element);
+    if (correct){
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+        // i can add an effect
+    }
+}
 
 
  
